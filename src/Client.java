2bf72d5650987470f6c1c8a39acf144483ccb185
kix92465 <kajohnson@dmacc.edu>
2017-11-18 18:16:24 -0600

    Client module completed with getters and setters

--- src/Client.java
+++ src/Client.java
@@ -7,4 +7,48 @@ public class Client
     //Check in time format yyyy-mm-dd hh:mm
     private String submittedTime;
 
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getPhone()
+    {
+        return phone;
+    }
+
+    public void setPhone(String phone)
+    {
+        this.phone = phone;
+    }
+
+    public String getSuspect()
+    {
+        return suspect;
+    }
+
+    public void setSuspect(String suspect)
+    {
+        this.suspect = suspect;
+    }
+
+    public String getSubmittedTime()
+    {
+        return submittedTime;
+    }
+
+    public void setSubmittedTime(String submittedTime)
+    {
+        this.submittedTime = submittedTime;
+    }
+
+    public String toString()
+    {
+        return "Client Name = " + name +  ", Phone = " + phone + ", Suspect = " + suspect + ", Submitted Time = " + submittedTime;
+    }
 }
