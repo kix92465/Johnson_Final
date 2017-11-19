db40682a9a1779403ae7e9f4714c102424841ac7
kix92465 <kajohnson@dmacc.edu>
2017-11-18 21:08:21 -0600

    work on repair module framework

--- src/Repair.java
+++ src/Repair.java
@@ -2,4 +2,71 @@ public class Repair
 {
     private Suspects inStoreSuspects;
     private Suspects serviceCenterSuspects;
+
+    public Repair()
+    {
+        inStoreSuspects = new Suspects();
+        serviceCenterSuspects = new Suspects();
+    }
+
+    public void addSuspect(Client client, boolean isServiceCenter)
+    {
+        if (isServiceCenter)
+            serviceCenterSuspects.push(client);
+        else
+            inStoreSuspects.push(client);
+    }
+
+    //do a sort before returning the next suspect because there can be a new suspect
+    //that has been entered into the system with an earlier appointment
+    public Client getNextInStoreRepairableSuspect()
+    {
+
+    }
+
+    //do a sort before returning the next suspect because there can be a new suspect
+    //that has been entered into the system with an earlier appointment
+    public Client getNextServiceCenterRepairableSuspect()
+    {
+
+    }
+
+    //do a sort before returning the next suspect because there can be a new suspect
+    //that has been entered into the system with an earlier appointment
+    public Client viewNextInStoreRepairableSuspect()
+    {
+
+    }
+
+    //do a sort before returning the next suspect because there can be a new suspect
+    //that has been entered into the system with an earlier appointment
+    public Client viewNextServiceCenterRepairableSuspect()
+    {
+
+    }
+
+    private static void sort(Suspects suspects)
+    {
+
+    }
+
+    public void printSortedInStoreSuspectList()
+    {
+
+    }
+
+    public void printSortedServiceCenterSuspectsList()
+    {
+
+    }
+
+    public void printInStoreSuspectsList()
+    {
+
+    }
+
+    public void printServiceCenterSuspectsList()
+    {
+
+    }
 }
--- src/Suspects.java
+++ src/Suspects.java
@@ -1,4 +1,5 @@
 import javax.xml.soap.Node;
+import java.util.concurrent.Callable;
 
 public class Suspects
 {
@@ -6,4 +7,46 @@ public class Suspects
     private int size = 0;
     //first pushed suspect
     private Node first = null;
+
+    private class Node
+    {
+        Client value;
+        Node next;
+
+        Node(Client value)
+        {
+            this.value =  value;
+            this.next = null;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "Node{" +
+                    "value=" + value +
+                    '}';
+        }
+    }
+
+    public Suspects push(Client value)
+    {
+        Node newNode = new Node(value);
+        newNode.next = first;
+        first = newNode;
+        return this;
+    }
+
+    //Deletes the top first value in the stack
+    //the value that has been deleted from the stack or null if the stack is empty
+
+    public Client pop()
+    {
+        if (first == null)
+            return null;
+        Client object = first.value;
+        first = first.next;
+        size--;
+        return object;
+    }
+
 }
